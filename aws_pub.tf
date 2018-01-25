eecbe6fab76ff3c29b2964e4b05b8f4c9a647724
DonSwizy <imad95370@gmail.com>
2018-01-25 10:29:46 +0100

    add rds
    add rds

--- aws_pub.tf
+++ aws_pub.tf
@@ -9,3 +9,14 @@ resource "aws_instance" "testweb" {
   ami           = "ami-41e0b93b"
   instance_type = "t2.micro"
 }
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.6.37"
+  instance_class       = "db.t2.micro"
+  name                 = "wordpress_db"
+  username             = "imad"
+  password             = "hajji"
+}
